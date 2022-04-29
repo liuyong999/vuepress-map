788d3705cac4397bc2e0ad8515a135273e122ed1
windows\liuyong <1228778532@qq.com>
2022-04-29 15:32:03 +0800

    Build the frame

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    title: 'VuePressMap',
+    description: '',
+    plugins: ['@vuepress/nprogress'],
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: '文化', link: '/culture/' },
+        { text: '教育', link: '/education/' },
+        { text: '政治', link: '/politics/' },
+        { text: '技术', link: '/technology/' },
+        { text: '医疗', link: '/medical/' },
+        { text: '国防', link: '/defense/' },
+        {
+          text: '学科',
+          items: [
+            { text: '数学', link: '/subject/math/' },
+            { text: '英语', link: '/subject/english/' }
+          ]
+        }
+      ],
+      smoothScroll: true
+    }
+  }
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+footer: MIT Licensed | Copyright © 2022
+---
+
+<!-- 医疗:预防、治疗 || 护理、急救(健康)  国防:武器 政治:政策 -->
+
