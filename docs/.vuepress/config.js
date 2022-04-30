a9cd00f19e48781c28384dac27adee06b0ef156a
windows\liuyong <1228778532@qq.com>
2022-04-30 18:23:31 +0800

    add file、description and modify config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,25 +4,56 @@ module.exports = {
     plugins: ['@vuepress/nprogress'],
     themeConfig: {
       nav: [
-        { text: 'Home', link: '/' },
-        { text: '文化', link: '/culture/' },
-        { text: '教育', link: '/education/' },
-        { text: '政治', link: '/politics/' },
-        { text: '技术', link: '/technology/' },
-        { text: '医疗', link: '/medical/' },
-        { text: '国防', link: '/defense/' },
+        { 
+          text: 'Home', 
+          link: '/' },
+        { 
+          text: '文化', 
+          link: '/culture/' 
+        },
+        { 
+          text: '经济', 
+          link: '/economy/' 
+        },
+        { 
+          text: '教育', 
+          link: '/education/' 
+        },
+        { 
+          text: '政治', 
+          link: '/politics/' 
+        },
+        { 
+          text: '技术', 
+          link: '/technology/' 
+        },
+        { 
+          text: '医疗', 
+          link: '/medical/' 
+        },
+        { 
+          text: '国防', 
+          link: '/defense/' 
+        },
         {
           text: '学科',
           items: [
-            { text: '数学', link: '/subject/math/' },
-            { text: '英语', link: '/subject/english/' }
+            { 
+              text: '数学', 
+              link: '/subject/math/' 
+            },
+            { 
+              text: '英语', 
+              link: '/subject/english/' 
+            }
           ]
         }
       ],
       smoothScroll: true,
       sidebar: {
         '/subject/english/': [
-          'word-analysis'
+          'word-analysis',
+          'incomprehensible-sentence'
         ]
       }
     }
---
+++ docs/technology/doc-map.md
@@ -0,0 +1,83 @@
+---
+title: 文档地图
+lang: zh-CN
+description: 开发经常使用的开发网站，开发工具
+---
+
+# {{ $frontmatter.title }}
+
+技术
+https://spring.io/
+https://mybatis.org/mybatis-3/
+https://vue3js.cn/
+https://nodejs.org/en/
+https://reactjs.org/
+https://www.jenkins.io/
+https://www.thymeleaf.org/index.html
+https://kubernetes.io/
+https://netty.io/index.html
+https://rocketmq.apache.org/
+https://www.rabbitmq.com/
+https://www.apache.org/
+https://git-scm.com/
+https://kafka.apache.org/
+https://nginx.org/en/
+https://www.mysql.com/
+http://openjdk.java.net/
+https://www.oracle.com/cn/index.html
+https://gradle.org/
+https://tomcat.apache.org/
+https://jwt.io/
+http://www.mycat.org.cn/
+https://www.elastic.co/cn/
+https://developers.weixin.qq.com/miniprogram/dev/framework/
+
+https://podman.io/
+https://www.docker.com/
+
+https://www.vmware.com/cn.html
+
+工具
+https://github.com/
+https://stackoverflow.com/
+https://leetcode-cn.com/
+https://www.lintcode.com/
+https://www.youtube.com/
+
+IDE
+https://www.jetbrains.com/
+https://code.visualstudio.com/
+
+
+blog
+https://martinfowler.com/
+
+期刊
+医学
+- https://www.nejm.org/
+- https://www.thelancet.com/
+- https://www.bmj.com/
+- https://jamanetwork.com/
+
+
+https://scholar.google.com
+https://arxiv.org/
+
+https://www.ieee.org//
+
+https://www.science.org/
+https://www.nature.com/
+
+
+http://webdocs.cs.ualberta.ca/~zaiane/htmldocs/ConfRanking.html
+
+
+
+https://www.linuxcool.com/
+https://redis.com.cn/
+https://www.processon.com/
+
+https://gitee.com/
+
+
+https://www.latex-project.org/
